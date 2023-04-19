af82324f699a657473944d3e17bc44a10bfdfc58
PIYUSH WADHAWAN <wa.piyush@gmail.com>
2023-04-19 20:58:52 +0530

    added input validation

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import './NewPlace.css'
+import Input from '../../shared/components/FormElements/Input'
+import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../../shared/components/util/validators'
 
 const NewPlace = () => {
+
+  const titleInputHandler = useCallback((id, value, isValid) => {}, [])    // useCallback used so that titlInputHandler doesn't cause an infinite rendering due to useEffect in Input.jsx 
+  
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, [])    // useCallback used so that descriptionInputHandler doesn't cause an infinite rendering due to useEffect in Input.jsx 
+
   return (
-    <h2>New place</h2>
+    <form className="place-form">
+      <Input 
+        id="title"
+        element="input" 
+        type="text" 
+        label="Title" 
+        validators={[VALIDATOR_REQUIRE()]} 
+        errorText="Please enter a valid title" 
+        onInput={titleInputHandler}/>
+
+<Input 
+        id="description"
+        element="textarea"  
+        label="Description" 
+        validators={[VALIDATOR_MINLENGTH(5)]} 
+        errorText="Minimum 5 words required" 
+        onInput={descriptionInputHandler}/>
+    </form>
   )
 }
 
